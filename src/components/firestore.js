55e68dd85b38c5261e87e80fd6cc7117f9a75955
pwwambui <pwwambui@safaricom.co.ke>
2025-03-02 15:59:50 +0300

    added the firestore file that connects to the database

---
+++ src/components/firestore.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs,addDoc } from "firebase/firestore";
+import { firestore } from "../firebase";
+
+function Firestore() {
+    const [projects, setProjects] = useState([]);
+
+    const fetchProjects = async () => {
+
+        const querySnapshot = await getDocs(collection(firestore, "projects"));
+        const fetchedProjects = querySnapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        }));
+      
+   
+    console.log(fetchedProjects);
+    setProjects(fetchedProjects);
+}
+
+        useEffect(() => {
+            fetchProjects();
+        }, [])
+
+        const addData = async () => {
+            try {
+            await addDoc(collection(firestore, "projects"), {
+                name: "wood work"
+              });
+              fetchProjects();
+            } catch (error) {
+                console.error ('error adding document', error);
+            }
+        }
+    
+
+    return(
+        <div>
+        <h1>Projects</h1>
+        <ul>
+            {projects.map((project) => (
+                <li key={project.id}>
+                    {project.name}
+                </li>
+            ))}
+        </ul>
+        <button onClick={addData}>Add Data</button>
+        </div>
+    )
+    
+}
+
+export default Firestore;
